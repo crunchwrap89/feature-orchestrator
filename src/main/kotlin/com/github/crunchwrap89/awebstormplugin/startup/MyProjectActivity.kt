90320cd234c35d019cf6ed1b8e9d6901896fe6aa
GitHub Action <action@github.com>
2025-12-16 20:33:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/crunchwrap89/awebstormplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/crunchwrap89/awebstormplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.crunchwrap89.awebstormplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
